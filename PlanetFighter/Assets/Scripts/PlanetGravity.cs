df9c2d2866136470776ecb14da84d8c56935b3c4
TKscoot <tkminecraftlp@gmail.com>
2017-11-01 22:10:47 +0100

    Uploaded Files

---
+++ PlanetFighter/Assets/Scripts/PlanetGravity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour {
+    [SerializeField]
+    Transform planet;
+    [SerializeField]
+    float gravity = 20f;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	public void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Vector2 difference = planet.transform.position - this.transform.position;
+        rb.AddForce((difference).normalized * gravity);
+        float angle = Mathf.Atan2(difference.x, difference.y);
+        this.transform.rotation = Quaternion.AngleAxis(angle, transform.up);
+	}
+
+    public void Attract(Rigidbody2D body)
+    {
+    }
+}
---
+++ PlanetFighter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float topSpeed = 10f;
+    
+    bool facingRight = true;            // player facing direction
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");       //get move direction
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if(move > 0 && !facingRight)
+        {
+            ChangeFacing();
+        }
+        else if(move < 0 && facingRight)
+        {
+            ChangeFacing();
+        }
+    }
+
+    void ChangeFacing()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+
+        theScale.x *= -1;
+
+        transform.localScale = theScale;
+    }
+}
